5b292b57ab9be61d808a661a533a47b3ce744714
Mozartoliveira10051987 <mozartoliveira10051987@gmail.com>
2021-09-15 18:01:29 -0300

    parOUimpar

---
+++ parOUimpar.js
@@ -0,0 +1,12 @@
+
+
+var limite = 34
+
+for(let valorAtual = 0; valorAtual <=limite; valorAtual++ )
+
+{
+    if (valorAtual === 0) { console.log("Zero") } 
+     else if (valorAtual % 2 ==0) { console.log(valorAtual + ": é Par") }
+        else { console.log(valorAtual + ": é Impar" ) }
+    
+}
